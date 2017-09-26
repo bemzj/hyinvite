e638a0f86948e1cdd61e98918e776e0498a985a5
Your Name <email@example.com>
2017-09-27 01:10:49 +0800

    110

--- js/index.js
+++ js/index.js
@@ -5,7 +5,27 @@ var loadImg = [
 ]
 //加载的图片
 var gameImg = [
-	{path:'img/logo.png',type:'img',name:'logo'},
+	{path:'img/check.png',type:'img',name:'check'},
+	{path:'img/loadBkg.jpg',type:'img',name:'loadBkg'},
+	{path:'img/map.png',type:'img',name:'map'},
+	{path:'img/pageOneBkg.jpg',type:'img',name:'pageOneBkg'},
+	{path:'img/pageOneTitle.png',type:'img',name:'pageOneTitle'},
+	{path:'img/personBkg.jpg',type:'img',name:'personBkg'},
+	{path:'img/up.png',type:'img',name:'up'},
+	{path:'img/videoOne.jpg',type:'img',name:'videoOne'},
+	{path:'img/banner01.jpg',type:'img',name:'banner01'},
+	{path:'img/banner02.jpg',type:'img',name:'banner02'},
+	{path:'img/banner03.jpg',type:'img',name:'banner03'},
+	{path:'img/btitle01.png',type:'img',name:'btitle01'},
+	{path:'img/btitle02.png',type:'img',name:'btitle02'},
+	{path:'img/btitle03.png',type:'img',name:'btitle03'},
+	{path:'img/bannerBottom.jpg',type:'img',name:'bannerBottom'},
+	{path:'img/date01.png',type:'img',name:'date01'},
+	{path:'img/date02.png',type:'img',name:'date02'},
+	{path:'img/date03.png',type:'img',name:'date03'},
+	{path:'img/dateImg.png',type:'img',name:'dateImg'},
+	{path:'img/pageFourBkg.jpg',type:'img',name:'pageFourBkg'},
+	
 ];
 //全局函数
 var backLayer,logoText,loadup;
@@ -62,10 +82,11 @@ function loading(progress){
 //开始
 function gameStart(){
 	backLayer.remove();
-//	$('#load').remove();
+	$('#load').remove();
 	$('#mainContent').show();//上下拨动
 	var swiper = new Swiper('.swiper-container-main', {
-	    direction: 'vertical'
+	    direction: 'vertical',
+	    initialSlide :2,
 	});
 	$('.person ul li input').on('touchstart',function(){
 		$(this).focus();
@@ -73,5 +94,52 @@ function gameStart(){
 	$('.person ul li select').on('touchstart',function(){
 		$(this).focus();
 		document.getElementById("s").size=document.getElementById("s").options.length;
+	});
+	$('#check').on('touchstart',function(){
+		if($(this).attr('check')=='true')
+		{
+			$(this).find('img').css('opacity','0');
+			$(this).siblings('div').css('color','#333333');
+			$(this).attr('check','false');
+		}else{
+			$(this).find('img').css('opacity','1.0');
+			$(this).siblings('div').css('color','#535353');
+			$(this).attr('check','true');
+		}	
+	});
+	//开始箭头向下动画
+	$('.up').animate({bottom:'6%',opacity:'0'},600,function(){
+		$(this).css('opacity',1);
+		$(this).css('bottom','10%');
+	});
+	var uptime = setInterval(function(){
+		$('.up').animate({bottom:'6%',opacity:'0'},600,function(){
+			$(this).css('opacity',1);
+			$(this).css('bottom','10%');
+		});
+	},600);
+	$('.up img').on('touchstart',function(){
+		swiper.slideNext();
+	});
+	var nexttime = setInterval(function(){
+		$('.next').animate({bottom:'2.5%',opacity:'0'},600,function(){
+			$(this).css('opacity',1);
+			$(this).css('bottom','5%');
+		});
+	},600);
+	$('.next img').on('touchstart',function(){
+		swiper.slideNext();
+	});
+	//轮播图
+	var banner = new Swiper('.swiper-container-banner', {
+	    direction: 'horizontal',
+	    autoplay : 5000,
+	    loop:true,
+	    autoplayDisableOnInteraction : false,
+	    pagination: '.swiper-pagination',
+	});
+	$('.bannerBottom').height(($(window).innerHeight()-$('.swiper-container-banner').height()));
+	$('#rightnow').on('touchstart',function(){
+		swiper.slideNext();
 	});
 }
